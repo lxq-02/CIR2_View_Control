fa3a2de45a58e5379ca52ca688a916125968cbcd
lixinqian <1512014058@qq.com>
2024-09-26 14:08:38 +0800

    将按钮部分也转换为信号

--- CIR_Control.cpp
+++ CIR_Control.cpp
@@ -25,20 +25,55 @@ CIR_Control::CIR_Control(QWidget* parent)
         << new QLabel(QStringLiteral("防护报警："))
         << new QLabel(QStringLiteral("列尾排风状态确认："))
         << new QLabel(QStringLiteral("故障黑屏："))
-        << new QLabel(QStringLiteral("软件显示隐藏："));
+        << new QLabel(QStringLiteral("软件显示隐藏："))
+        << new QLabel(QStringLiteral("TopBtn1："))
+        << new QLabel(QStringLiteral("TopBtn2："))
+        << new QLabel(QStringLiteral("TopBtn3："))
+        << new QLabel(QStringLiteral("TopBtn4："))
+        << new QLabel(QStringLiteral("TopBtn5："))
+        << new QLabel(QStringLiteral("TopBtn6："))
+        << new QLabel(QStringLiteral("TopBtn7："))
+        << new QLabel(QStringLiteral("TopBtn8："))
+        << new QLabel(QStringLiteral("LeftBtn1："))
+        << new QLabel(QStringLiteral("LeftBtn2："))
+        << new QLabel(QStringLiteral("LeftBtn3："))
+        << new QLabel(QStringLiteral("LeftBtn4："))
+        << new QLabel(QStringLiteral("LeftBtn5："))
+        << new QLabel(QStringLiteral("MidBtnCancelNum："))
+        << new QLabel(QStringLiteral("MidBtnConfirm："))
+        << new QLabel(QStringLiteral("MidBtnAirpressCheck："))
+        << new QLabel(QStringLiteral("MidBtn1："))
+        << new QLabel(QStringLiteral("MidBtn2："))
+        << new QLabel(QStringLiteral("MidBtn3："))
+        << new QLabel(QStringLiteral("MidBtn4："))
+        << new QLabel(QStringLiteral("MidBtn5："))
+        << new QLabel(QStringLiteral("MidBtn6："))
+        << new QLabel(QStringLiteral("MidBtn7："))
+        << new QLabel(QStringLiteral("MidBtn8："))
+        << new QLabel(QStringLiteral("MidBtn9："))
+        << new QLabel(QStringLiteral("MidBtnStart："))
+        << new QLabel(QStringLiteral("MidBtn0："))
+        << new QLabel(QStringLiteral("MidBtnHash："))
+        << new QLabel(QStringLiteral("MidBtnCall："))			    
+        << new QLabel(QStringLiteral("MidBtnSwitch："))			
+        << new QLabel(QStringLiteral("MidBtnHangUp："))			
+        << new QLabel(QStringLiteral("MidBtnSettings："))		    
+        << new QLabel(QStringLiteral("MidBtnup："))				
+        << new QLabel(QStringLiteral("MidBtnInterface："))		
+        << new QLabel(QStringLiteral("MidBtnLeft："))			   
+        << new QLabel(QStringLiteral("MidBtnConfirm："))		   
+        << new QLabel(QStringLiteral("MidBtnRight："))			
+        << new QLabel(QStringLiteral("MidBtnSearch："))		  
+        << new QLabel(QStringLiteral("MidBtnDown："))			  
+        << new QLabel(QStringLiteral("MidBtnBackspace："))		
+        << new QLabel(QStringLiteral("MidBtnPrint："))			
+        << new QLabel(QStringLiteral("MidBtnShuntingRequest：")) 
+        << new QLabel(QStringLiteral("MidBtnExit："));
 
-    sendCombos << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox()
-        << new QComboBox();
+    for (int i = 0; i < sendLabels.size(); i++)
+    {
+        sendCombos << new QComboBox();
+    }
 
     // 初始化每个组合框的选项为 0 和 1
     for (auto combo : sendCombos) {
@@ -83,6 +118,10 @@ CIR_Control::CIR_Control(QWidget* parent)
 
     QVBoxLayout* recvLayout = new QVBoxLayout();
     QVBoxLayout* sendLayout = new QVBoxLayout();
+    QVBoxLayout* topLayout = new QVBoxLayout();
+    QVBoxLayout* leftLayout = new QVBoxLayout();
+    QVBoxLayout* midLayout = new QVBoxLayout();
+    QVBoxLayout* rightLayout = new QVBoxLayout();
 
     for (int i = 0; i < recvLabels.size(); ++i) {
         QHBoxLayout* hLayout = new QHBoxLayout();
@@ -91,15 +130,48 @@ CIR_Control::CIR_Control(QWidget* parent)
         recvLayout->addLayout(hLayout);
     }
 
-    for (int i = 0; i < sendLabels.size(); ++i) {
+    for (int i = 0; i < 12; ++i) 
+    {
         QHBoxLayout* hLayout = new QHBoxLayout();
         hLayout->addWidget(sendLabels[i]);
         hLayout->addWidget(sendCombos[i]);
         sendLayout->addLayout(hLayout);
     }
+    for (int i = 12; i < 20; ++i)
+    {
+        QHBoxLayout* hLayout = new QHBoxLayout();
+        hLayout->addWidget(sendLabels[i]);
+        hLayout->addWidget(sendCombos[i]);
+        topLayout->addLayout(hLayout);
+    }
+    for (int i = 20; i < 25; ++i)
+    {
+        QHBoxLayout* hLayout = new QHBoxLayout();
+        hLayout->addWidget(sendLabels[i]);
+        hLayout->addWidget(sendCombos[i]);
+        leftLayout->addLayout(hLayout);
+    }
+    for (int i = 25; i < 40; ++i)
+    {
+        QHBoxLayout* hLayout = new QHBoxLayout();
+        hLayout->addWidget(sendLabels[i]);
+        hLayout->addWidget(sendCombos[i]);
+        midLayout->addLayout(hLayout);
+    }
+    for (int i = 40; i < 55; ++i)
+    {
+        QHBoxLayout* hLayout = new QHBoxLayout();
+        hLayout->addWidget(sendLabels[i]);
+        hLayout->addWidget(sendCombos[i]);
+        rightLayout->addLayout(hLayout);
+    }
 
     QHBoxLayout* mainLayout = new QHBoxLayout();
     mainLayout->addLayout(sendLayout);
+    mainLayout->addLayout(topLayout);
+    mainLayout->addLayout(leftLayout);
+    mainLayout->addLayout(midLayout);
+    mainLayout->addLayout(rightLayout);
     mainLayout->addLayout(recvLayout);
 
     QWidget* centralWidget = new QWidget();
@@ -159,4 +231,51 @@ void CIR_Control::updateSend()
     m_dataSend.MC_AIRDISCHARGE = sendCombos[9]->currentIndex();
     m_dataSend.MC_MALFUNCTION = sendCombos[10]->currentIndex();
     m_dataSend.MC_SHOWorHIDE = sendCombos[11]->currentIndex();
+
+    m_dataSend.MC_TOPBTN1 = sendCombos[12]->currentIndex();
+    m_dataSend.MC_TOPBTN2 = sendCombos[13]->currentIndex();
+    m_dataSend.MC_TOPBTN3 = sendCombos[14]->currentIndex();
+    m_dataSend.MC_TOPBTN4 = sendCombos[15]->currentIndex();
+    m_dataSend.MC_TOPBTN5 = sendCombos[16]->currentIndex();
+    m_dataSend.MC_TOPBTN6 = sendCombos[17]->currentIndex();
+    m_dataSend.MC_TOPBTN7 = sendCombos[18]->currentIndex();
+    m_dataSend.MC_TOPBTN8 = sendCombos[19]->currentIndex();
+
+    m_dataSend.MC_LEFTBTN_AIRDISCHARGE = sendCombos[20]->currentIndex();
+    m_dataSend.MC_LEFTBTN_EMERGENCYCALL = sendCombos[21]->currentIndex();
+    m_dataSend.MC_LEFTBTN_WARNING = sendCombos[22]->currentIndex();
+    m_dataSend.MC_LEFTBTN_MAINCONTROL = sendCombos[23]->currentIndex();
+    m_dataSend.MC_LEFTBTN_RESET = sendCombos[24]->currentIndex();
+
+    m_dataSend.MC_MIDBTN_CANCELNUM = sendCombos[25]->currentIndex();
+    m_dataSend.MC_MIDBTN_CONFIRM = sendCombos[26]->currentIndex();
+    m_dataSend.MC_MIDBTN_AIRPRESSCHECK = sendCombos[27]->currentIndex();
+    m_dataSend.MC_MIDBTN_1 = sendCombos[28]->currentIndex();
+    m_dataSend.MC_MIDBTN_2 = sendCombos[29]->currentIndex();
+    m_dataSend.MC_MIDBTN_3 = sendCombos[30]->currentIndex();
+    m_dataSend.MC_MIDBTN_4 = sendCombos[31]->currentIndex();
+    m_dataSend.MC_MIDBTN_5 = sendCombos[32]->currentIndex();
+    m_dataSend.MC_MIDBTN_6 = sendCombos[33]->currentIndex();
+    m_dataSend.MC_MIDBTN_7 = sendCombos[34]->currentIndex();
+    m_dataSend.MC_MIDBTN_8 = sendCombos[35]->currentIndex();
+    m_dataSend.MC_MIDBTN_9 = sendCombos[36]->currentIndex();
+    m_dataSend.MC_MIDBTN_STAR = sendCombos[37]->currentIndex();
+    m_dataSend.MC_MIDBTN_0 = sendCombos[38]->currentIndex();
+    m_dataSend.MC_MIDBTN_HASH = sendCombos[39]->currentIndex();
+
+    m_dataSend.MC_RIGHTBTN_CALL = sendCombos[40]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_SWITCH = sendCombos[41]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_HANGUP = sendCombos[42]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_SETTINGS = sendCombos[43]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_UP = sendCombos[44]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_INTERFACE = sendCombos[45]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_LEFT = sendCombos[46]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_CONFIRM = sendCombos[47]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_RIGHT = sendCombos[48]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_SEARCH = sendCombos[49]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_DOWN = sendCombos[50]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_BACKSPACE = sendCombos[51]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_PRINT = sendCombos[52]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_SHUNTINGREQUEST = sendCombos[53]->currentIndex();
+    m_dataSend.MC_RIGHTBTN_EXIT = sendCombos[54]->currentIndex();
 }
--- struct.h
+++ struct.h
@@ -15,6 +15,53 @@ typedef struct _tagDATASEND
 	int	MC_AIRDISCHARGE;//列尾排风状态确认0 无1结束  语音播报风压注意
 	int MC_MALFUNCTION;	//故障黑屏0无1黑屏
 	int	MC_SHOWorHIDE;	//软件显示隐藏 0 隐藏 1显示
+
+	int MC_TOPBTN1;
+	int MC_TOPBTN2;
+	int MC_TOPBTN3;
+	int MC_TOPBTN4;
+	int MC_TOPBTN5;
+	int MC_TOPBTN6;
+	int MC_TOPBTN7;
+	int MC_TOPBTN8;
+
+	int MC_LEFTBTN_AIRDISCHARGE;	// 列为排风
+	int MC_LEFTBTN_EMERGENCYCALL;	// 紧急呼叫
+	int MC_LEFTBTN_WARNING;			// 报警
+	int MC_LEFTBTN_MAINCONTROL;		// 主控
+	int MC_LEFTBTN_RESET;			// 复位
+
+	int MC_MIDBTN_CANCELNUM;		// 列尾消号
+	int MC_MIDBTN_CONFIRM;			// 列尾确认
+	int MC_MIDBTN_AIRPRESSCHECK;	// 风压查询
+	int MC_MIDBTN_1;				// 数字1
+	int MC_MIDBTN_2;				// 数字2
+	int MC_MIDBTN_3;				// 数字3
+	int MC_MIDBTN_4;				// 数字4
+	int MC_MIDBTN_5;				// 数字5
+	int MC_MIDBTN_6;				// 数字6
+	int MC_MIDBTN_7;				// 数字7
+	int MC_MIDBTN_8;				// 数字8
+	int MC_MIDBTN_9;				// 数字9
+	int MC_MIDBTN_STAR;				// 字符*
+	int MC_MIDBTN_0;				// 数字0
+	int MC_MIDBTN_HASH;				// 字符#
+
+	int MC_RIGHTBTN_CALL;			// 呼叫
+	int MC_RIGHTBTN_SWITCH;			// 切换
+	int MC_RIGHTBTN_HANGUP;			// 挂断
+	int MC_RIGHTBTN_SETTINGS;		// 设置
+	int MC_RIGHTBTN_UP;				// 向上↑
+	int MC_RIGHTBTN_INTERFACE;		// 界面
+	int MC_RIGHTBTN_LEFT;			// 向左←
+	int MC_RIGHTBTN_CONFIRM;		// 确认
+	int MC_RIGHTBTN_RIGHT;			// 向右→
+	int MC_RIGHTBTN_SEARCH;		// 查询
+	int MC_RIGHTBTN_DOWN;			// 向下↓
+	int MC_RIGHTBTN_BACKSPACE;		// 回格
+	int MC_RIGHTBTN_PRINT;			// 打印
+	int MC_RIGHTBTN_SHUNTINGREQUEST;// 调车请求
+	int MC_RIGHTBTN_EXIT;			// 推出
 }DATASEND;
 
 // 接收的各种数据
